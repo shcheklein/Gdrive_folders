5343f1391dd0bdd1dc814b25a57f015b9284f505
Fabian Cudis <fabclub747@gmail.com>
2021-02-03 07:32:45 -0400

    fix to settings

--- consultora_excel/settings.py
+++ consultora_excel/settings.py
@@ -83,20 +83,14 @@ WSGI_APPLICATION = 'consultora_excel.wsgi.application'
 
 DATABASES = {
     'default': {
+             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+             'NAME': 'consultora_excel',
+             'USER': 'postgres',
+             'PASSWORD': 'postgres',
+             'HOST': '127.0.0.1',
+             'PORT': '5432'
 
-        'ENGINE': 'django.db.backends.sqlite3',
-
-        'NAME': 'db.sqlite3',
-
-        #'USER': 'postgres',
-
-        #'PASSWORD': 'postgres',
-
-        #'HOST': '127.0.0.1',
-
-        #'PORT': '5432',
-
-    }
+         }
 }
 
 Q_CLUSTER = {
