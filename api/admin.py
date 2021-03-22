69ea57c9ee719672697e4e5123d18071ea849b47
Fabian Cudis <fabclub747@gmail.com>
2021-03-22 18:52:20 -0400

    new model created

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Config
+from .models import Config, Cliente, Tarea
 #from solo.admin import SingletonModelAdmin
 
 admin.site.register(Config)
+admin.site.register(Cliente)
+admin.site.register(Tarea)
 # Register your models here.
--- api/models.py
+++ api/models.py
@@ -15,3 +15,29 @@ class Config(SingletonModel):
     remito_col = models.CharField(max_length=256, blank=True, verbose_name="Numero de columna remito")
     informe_col = models.CharField(max_length=256, blank=True, verbose_name="Numero de columna informe")
     relevamientos_col = models.CharField(max_length=256, blank=True, verbose_name="Numero de columna relevamientos")
+
+
+class Cliente(models.Model):
+
+    nombre = models.CharField(max_length=300)
+    correo1 = models.EmailField(blank=True, null=True)
+    correo2 = models.EmailField(blank=True, null=True)
+    correo3 = models.EmailField(blank=True, null=True)
+    correo4 = models.EmailField(blank=True, null=True)
+    correo5 = models.EmailField(blank=True, null=True)
+    correo6 = models.EmailField(blank=True, null=True)
+    id_cliente = models.CharField(max_length=50, unique=True, primary_key=True)
+
+
+class Tarea (models.Model):
+    remito = models.CharField(max_length=100, unique=True, primary_key=True)
+    id_cliente = models.CharField(max_length=100)
+    tipo_estudio = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(null=True, blank=True)
+    is_send = models.BooleanField(default=False)
+    aprobado2 = models.CharField(max_length=300)
+
+
+
+
+
